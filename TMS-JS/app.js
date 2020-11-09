d3c76bb152fa177c86180661c09c98695e1a3cc7
Delendik Ivan <vanya.delya@mail.ru>
2020-11-09 21:31:39 +0300

    Home work 1

--- TMS-JS/app.js
+++ TMS-JS/app.js
@@ -1,1 +1,41 @@
-alert(1)
+// A)
+
+let v = 'First var';
+alert(v);
+v = 'Second var';
+alert(v)
+
+// B)
+
+let integer = 13;
+let agree = true;
+let und = undefined;
+let nu = null;
+alert(integer+ ' ' + agree + ' ' + und + ' ' + nu);
+
+let obj = {};
+obj.name = 'Ivan';
+obj.age = 26;
+let mas = ['apple', 'banana', 'orange'];
+console.log(obj);
+console.log(mas);
+
+// С)
+
+const age = prompt('Каков ваш возраст?');
+if (age == '' || age == null) {
+        alert('Поздравляем, вам 0 полных лет!!!');
+    }
+    else {
+        alert('Поздравляем, вам '+ age +' полных лет!!!');
+    }
+
+if (age >=18) {
+        alert('Вы совершеннолетний!');
+    }
+    else {
+        alert('Вы несовершеннолетний!');
+    }
+
+
+
